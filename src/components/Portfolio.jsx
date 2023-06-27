bfa1e48a9629a132f40f2121a71b9f452a257991
Fiterman <gfiterman96@gmail.com>
2023-06-27 13:10:33 -0300

    add floating highlight projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -25,11 +25,47 @@ function PortfolioGallery() {
     </ul>
   );
 }
+function PortfolioHighlight({ projectNames }) {
+  return (
+    <div className="Portfolio__highlight">
+      {porfolioData
+        .filter((project) => projectNames.includes(project.company.name))
+        .map((project, index) => (
+          <div key={project.id} className={`Portfolio__highlight--card ${index === 0 ? "card1" : "card2"}`}>
+              <div className="Portfolio__highlight--card--heading">
+                  <img 
+                    className="Portfolio__highlight--heading--logo"
+                    src={`/public/img/portfolio/${project.company.logoFile}`}
+                    alt={`Logo da empresa ${project.company.name}`}
+                  />
+                  <h3 className="title Portfolio__highlight--heading--title">{project.company.name}</h3>
+              </div>
+              <img
+                className="Portfolio__highlight--thumbnail"
+                src={`/public/img/portfolio/${project.general.thumbnailName}${project.general.thumbnailExtension}`}
+                alt={`Card para o projeto ${project.company.name}`}
+              />
+          </div>
+        ))}
+    </div>
+  );
+}
 
 export default function Portfolio() {
+  const highlihtProjects = ["Go Best Life", "ByShop"];
+  const astronautTouchMeSrc = '/public/img/astronauts/Astronaut_Genial+Switch-Aceso.png'
+  const badgeTouchMeSrc = '/public/img/astronauts/Balao_Dialogo.png';
+
   return (
     <div className="Portfolio">
-      <h1 className="title title-big Portfolio--title">{_t.title}</h1>
+      <div className="d-flex align-items-center justify-content-between">
+        <h1 className="title title-big Portfolio--title">{_t.title}</h1>
+        <div className="d-flex align-items-center">
+          <img className="Portfolio--image--badge" alt="Astronauta clique-me" src={badgeTouchMeSrc}/>
+          <img className="Portfolio--image--astronaut" alt="Astronauta clique-me" src={astronautTouchMeSrc}/>
+        </div>
+      </div>
+      <PortfolioHighlight projectNames={highlihtProjects} />
       <PortfolioGallery />
     </div>
   );
