91f82786e20eafe7a515e1e01cdecd38bc006766
Fiterman <gfiterman96@gmail.com>
2023-06-28 15:32:14 -0300

    created basic structure for Projects pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import "./scss/app.scss";
 import Portfolio from "./components/Portfolio";
 import Root from "./routes/Root";
 import ErrorPage from "./pages/ErrorPage";
-import Project from "./components/Project";
+import Project from "./pages/Project";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />
+    element: <Root />
+    // errorElement: <ErrorPage />
   },
-  {
-    path: "/portfolio",
-    element: <Portfolio />,
-    errorElement: <ErrorPage />
-  },
+  // {
+    // path: "/portfolio",
+    // element: <Portfolio />
+    // errorElement: <ErrorPage />
+  // },
   {
     path: "/portfolio/:id",
     element: <Project />
