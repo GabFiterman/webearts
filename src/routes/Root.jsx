01adbf50674219dbbc79af04c4298ed3b63606bc
Fiterman <gfiterman96@gmail.com>
2023-06-28 15:35:18 -0300

    Revert "add Footer component"
    This reverts commit 2df868db4f2dbd62aa631766273b94d0dd71f770.

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -8,7 +8,6 @@ import Services from "../components/Services";
 import Portfolio from "../components/Portfolio"
 import Contact from "../components/Contact";
 import Team from '../components/Team';
-import Footer from '../components/Footer';
 
 export default function Root() {
   return (
@@ -41,11 +40,6 @@ export default function Root() {
       <Row>
         <Col className="px-0 mt-5">
           <Team />
-        </Col>
-      </Row>
-      <Row>
-        <Col className="px-0 mt-5">
-          <Footer />
         </Col>
       </Row>
     </Container>
