995e8ab83109c89e6af026e459cef5b7525ff032
Fiterman <gfiterman96@gmail.com>
2023-06-29 21:24:05 -0300

    style Heading main ajusts

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,24 +1,23 @@
+import { Col, Container, Row } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import ReactHtmlParser from "react-html-parser";
 import "../scss/heading.scss";
-import { Col, Container, Row } from "react-bootstrap";
-// import textMain from '../data/text-main.json'
 
 function FadeText({ texts, interval }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === texts.length - 1 ? 0 : prevIndex + 1
-      );
+      setCurrentIndex((prevIndex) => {
+        return prevIndex === texts.length - 1 ? 0 : prevIndex + 1;
+      });
     }, interval);
 
     return () => clearInterval(timer);
   }, [texts, interval]);
 
   return (
-    <Col className="FadeText__col">
+    <Col>
       {texts.map((text, index) => (
         <div
           key={index}
@@ -36,20 +35,22 @@ function FadeText({ texts, interval }) {
 function FunnyImage() {
   const imageSrc = "/img/StickerEinstein.png";
   return (
-    <img
-      className="FunnyImage__image--animation"
-      src={imageSrc}
-      alt="Sticker Albert Einstein"
-    />
+    <>
+      <img
+        className="FunnyImage__image--animation"
+        src={imageSrc}
+        alt="Sticker Albert Einstein"
+      />
+    </>
   );
 }
 
 export default function Heading({ _t }) {
   console.log("Heading Says: \n _t.bodyText:\n", _t.bodyText);
   return (
-    <Container fluid>
+    <Container fluid className="Heading">
       <Row>
-        <Col className="px-0 mt-5">
+        <Col xs={12} md={8} lg={6}>
           <h1 className="title title-big firstTitle--animation">
             {ReactHtmlParser(_t.title)}
           </h1>
@@ -61,11 +62,11 @@ export default function Heading({ _t }) {
         </Col>
       </Row>
       <Row>
-        <Col className="FunnyImage__col">
+        <Col className="FunnyImage__col" xs={12}>
           <FunnyImage />
         </Col>
       </Row>
-      <Row>
+      <Row xs={12} md={8} lg={7}>
         <FadeText texts={_t.bodyText} interval={_t.bodyTextInterval} />
       </Row>
     </Container>
