15ad37421aae02d8cf26f433f3033af21cf7ab87
Fiterman <gfiterman96@gmail.com>
2023-07-19 11:56:38 -0300

    add Portfolio Pages

---
+++ src/components/PortfolioDesign.jsx
@@ -0,0 +1,7 @@
+export default function PortfolioDesign ({_t}) {
+    return (
+        <div>
+            <h1>Portfolio DESIGN</h1>
+        </div>
+    )
+}
---
+++ src/components/PortfolioDev.jsx
@@ -0,0 +1,7 @@
+export default function PortfolioDev ({_t}) {
+    return (
+        <div>
+            <h1>Portfolio DESENVOLVIMENTO</h1>
+        </div>
+    )
+}
