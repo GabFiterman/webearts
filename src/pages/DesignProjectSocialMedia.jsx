6b13b78caa1c0c7f83d193338bdb3a4c20e1bd2f
Fiterman <gfiterman96@gmail.com>
2023-07-25 13:05:52 -0300

    add design categories full frontend and routing

---
+++ src/pages/DesignProjectSocialMedia.jsx
@@ -0,0 +1,13 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+export default function DesignProjectSocialMedia() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>DesignProjectSocialMedia</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
