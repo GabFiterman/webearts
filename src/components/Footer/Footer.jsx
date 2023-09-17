b583070a573d36007b561118be3085dbc5f31e30
Fiterman <gfiterman96@gmail.com>
2023-09-17 02:17:02 -0300

    feat: copyToClipboard create component: CopyToClipboard button Add contacts to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,32 @@
 import './footer.scss';
+import { CopyToClipboard } from '@/components';
 
 export default function Footer({ _t }) {
     const logoSrc = '/img/Logo-DarkMode.webp';
+    const contactList = {
+        whatsapp_link: 'https://api.whatsapp.com/send?phone=5562984602348',
+        email: 'gabriel@webearts.com',
+    };
+
     return (
         <div className="Footer">
-            <img className="Footer__logo" src={logoSrc} alt="Logo WebeArts" />
-            <h2 className="Footer__title title">{_t.title}</h2>
+            <div className="Footer__info">
+                <img
+                    className="Footer__logo"
+                    src={logoSrc}
+                    alt="Logo WebeArts"
+                />
+            </div>
+
+            <div className="Footer__contact">
+                <button className="CopyTo__button">
+                    <a href={contactList.whatsapp_link} target="_blank">
+                        Whatsapp
+                    </a>
+                </button>
+                <CopyToClipboard textToCopy={contactList.email} />
+            </div>
+
             <p className="Footer__subtitle">{_t.subtitle}</p>
         </div>
     );
