5eeaab50a9a8df6ab5155ce4c91e3fcc5a067054
Fiterman <gfiterman96@gmail.com>
2024-01-22 14:39:02 -0300

    patch: remove Developer Portfolio and show only Design portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,38 +1,41 @@
 // import '../scss/portfolio.scss';
-import { useState } from 'react';
+// import { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import PortfolioDev from './PortfolioDev';
+// import PortfolioDev from './PortfolioDev';
 import PortfolioDesign from './PortfolioDesign';
 
 function CategoriesContainer(props) {
     const { _t } = props;
-    const categories = _t.categories;
-    const [activeCategory, setActiveCategory] = useState(categories[1]);
+    // const categories = _t.categories;
+    // const [activeCategory, setActiveCategory] = useState(categories[1]);
 
-    return (
-        <>
-            <div className='CategoriesContainer mt-3'>
-                {categories.map((category, i) => (
-                    <h3
-                        className={
-                            category === activeCategory
-                                ? 'activeCategory category__title'
-                                : 'category__title'
-                        }
-                        key={i}
-                        onClick={() => setActiveCategory(category)}
-                    >
-                        {category}
-                    </h3>
-                ))}
+	return (
+		<>
+			<div className='CategoriesContainer mt-3'>
+				<PortfolioDesign _t={_t} />
             </div>
-            {activeCategory === 'desenvolvimento' ? (
-                <PortfolioDev _t={_t} />
-            ) : (
-                <PortfolioDesign _t={_t} />
-            )}
-        </>
-    );
+                {/* NOTE: Portfolio de Dev e Design */}
+				{/* {categories.map((category, i) => (
+					<h3
+						className={
+							category === activeCategory
+								? 'activeCategory category__title'
+								: 'category__title'
+						}
+						key={i}
+						onClick={() => setActiveCategory(category)}
+					>
+						{category}
+					</h3>
+				))}
+			</div>
+			{activeCategory === 'desenvolvimento' ? (
+				<PortfolioDev _t={_t} />
+			) : (
+				<PortfolioDesign _t={_t} />
+			)} */}
+		</>
+	);
 }
 export default function Portfolio({ _t }) {
     const astronautFlying = '/img/astronauts/Astronaut_LoveU.webp';
